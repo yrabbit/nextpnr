acbff2e686f570f5d5cce4bb01a0a185e0490f7b
YRabbit <rabbit@yrabbit.cyou>
2025-07-06 10:47:39 +1000

    wip

--- himbaechel/uarch/gowin/gowin.cc
+++ himbaechel/uarch/gowin/gowin.cc
@@ -180,6 +180,7 @@ void GowinImpl::init(Context *ctx)
         ctx->set_speed_grade(match[2]);
     } else {
         if (pn.length() > 2 && pn.compare(pn.length() - 2, 2, "ES")) {
+			log("%s\n", pn.c_str());
             package_idx = ctx->id(pn);
             spd = ctx->id("ES");
             ctx->set_speed_grade("ES");
