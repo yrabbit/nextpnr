b0d6b97936a808241ffb05d9faff837ebfe6aa08
Miodrag Milanović <mmicko@gmail.com>
2026-02-25 10:05:36 +0100

    gatemate: floorplanning [sc-168] (#1607)
    * gatemate: add CCF floorplanning parser
    
    * apply constraints
    
    * cleanup
    
    * print detected region and error if not found
    
    * Add wildcard matching
    
    * Validate placebox and use official coordinate system
    
    * Fix some messages

--- himbaechel/uarch/gatemate/gatemate.h
+++ himbaechel/uarch/gatemate/gatemate.h
@@ -115,6 +115,8 @@ struct GateMateImpl : HimbaechelAPI
     dict<int, IdString> index_to_die;
     dict<IdString, int> die_to_index;
     dict<IdString, dict<IdString, IdString>> pass_backtrace;
+    pool<IdString> scopenames;
+    pool<IdString> scopenames_used;
 
   private:
     bool getChildPlacement(const BaseClusterInfo *cluster, Loc root_loc,
